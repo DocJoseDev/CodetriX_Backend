67ac719456ffa601f4df092d50570861c131b24a
DocJoseDev <{correo@ejemplo.com}>
2026-04-14 15:42:58 -0600

    Cambios en la rama master, se agrego la configuracion DNS

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,11 @@
 import mongoose from 'mongoose';
+import dns from 'node:dns';
 
 const connectDB = async () => {
   try {
+    dns.setServers(['8.8.8.8']);
+    console.log('DNS configurado a 8.8.8.8');
+
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
--- src/routes/codigo.routes.js
+++ src/routes/codigo.routes.js
@@ -5,4 +5,12 @@ const router = express.Router();
 
 router.get('/codigov1', getCodigoV1);
 
+router.get('/nava', (req, res ) => {
+    res.status(200).json({
+        endpoint: 'nava',
+        message: 'Are you talking to me !!',
+        timestamp: new Date().toISOString()
+    })
+});
+
 export default router;
