622508a5b4c77d3cb8e3290929a748911dbff36a
Carlo Amezcua <carlo.141592@gmail.com>
2026-04-27 09:42:17 -0600

    Add captcha generation and verification routes

--- src/routes/codigo.routes.js
+++ src/routes/codigo.routes.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { getCodigoV1 } from '../controllers/codigo.controller.js';
+import { generateCaptcha, verifyCaptcha } from '../controllers/captcha.controller.js';
 
 const router = express.Router();
 
 router.get('/codigov1', getCodigoV1);
+router.get('/captcha', generateCaptcha);
+router.post('/captcha/verify', verifyCaptcha);
 
 router.get('/nava', (req, res ) => {
     res.status(200).json({
